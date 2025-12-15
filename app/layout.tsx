c3e39e1876034d3754d0bbf6c09033d99f9a6aa8
DDH <108775906+DDH-WEB@users.noreply.github.com>
2025-12-15 16:58:52 -0500

    Update location in openGraph description

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export const metadata: Metadata = {
     type: "website",
     locale: "es_PE",
     title: "Ferretería William David - Herramientas de Alta Calidad",
-    description: "Encuentra herramientas y materiales de construcción de alta calidad en CP 19 de Agosto, Perú",
+    description: "Encuentra herramientas y materiales de construcción de alta calidad en CP 19 de Agosto, La Unión",
     siteName: "Ferretería William David",
   },
   generator: "DDH DEV",
